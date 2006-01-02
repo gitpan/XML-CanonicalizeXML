807dcc102732e5f3478a14bc10d12fda448bed87
Stefan Zasada <sjz@NOSPAM.zasada.co.uk>
2006-01-02 17:17:13 +0000

    Import of SJZASADA/XML-CanonicalizeXML-0.03 from CPAN.
    gitpan-cpan-distribution: XML-CanonicalizeXML
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SJZASADA/XML-CanonicalizeXML-0.03.tar.gz
    gitpan-cpan-author:       SJZASADA
    gitpan-cpan-maturity:     released

--- lib/XML/CanonicalizeXML.pm
+++ lib/XML/CanonicalizeXML.pm
@@ -1,6 +1,5 @@
 package XML::CanonicalizeXML;
 
-use 5.008003;
 use strict;
 use warnings;
 use Carp;
@@ -27,7 +26,7 @@ our @EXPORT = qw(
 	
 );
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
